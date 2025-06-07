a97df1002f10a1f2b4d36c8cc6292cdd526b05e9
Ndubuisi JNR <ukonu039@gmail.com>
2025-06-07 09:17:57 -0700

    updated  notifications

--- src/pages/auth/Login.vue
+++ src/pages/auth/Login.vue
@@ -122,7 +122,7 @@ onMounted(() => {
                         class="text-3xl font-bold bg-gradient-to-r from-[#F97316] to-[#2563EB] text-transparent bg-clip-text inline-block mb-4">
                         Aza-pal
                     </a>
-                    <h1 class="text-2xl font-bold text-gray-900 mb-2">Welcome Back</h1>
+                    <h1 class="text-2xl font-bold text-gray-900 mb-2">Welcome Back.</h1>
                     <p class="text-gray-600 text-sm">
                         Social Commerce Made Easy.
                     </p>
