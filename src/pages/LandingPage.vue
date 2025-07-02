b9e0a6257ccf3161909e539052c5f2f9c6e4ef9f
ennovatingx <ukonu039@gmail.com>
2025-07-02 15:48:35 +0100

    update

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -2,9 +2,9 @@
 import Hero from '../components/Hero.vue';
 import Navbar from '../components/Navbar.vue';
 import Feature from '../components/Feature.vue';
-import HowItWorks from '../components/HowItWorks.vue';
-import Security from '../components/Security.vue';
-import CTA from '../components/CTA.vue';
+// import HowItWorks from '../components/HowItWorks.vue';
+// import Security from '../components/Security.vue';
+// import CTA from '../components/CTA.vue';
 import Footer from '../components/Footer.vue';
 </script>
 
